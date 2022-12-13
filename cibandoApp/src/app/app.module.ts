138d3a768f37e3629a4940f42c5c37e7e9bcb5f9
Carlo Mazzotta <ci.mazzotta.esterno@almaviva.it>
2022-12-13 18:04:06 +0100

    osservabili

--- cibandoApp/src/app/app.module.ts
+++ cibandoApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './shared/carousel/carousel.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ cibandoApp/src/app/models/recipe.model.ts
@@ -0,0 +1,9 @@
+export interface Recipe {
+    _id: number;
+    title: string;
+    description: string;
+    difficulty: number;
+    image: string;
+    date: string;
+    published: boolean;
+}
